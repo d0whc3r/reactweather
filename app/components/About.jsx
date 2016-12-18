13326ea81ac99be92bb421886bcaa6d2c25f7457
user <user@debian>
2016-12-18 09:34:46 +0100

    about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,17 @@ console.log($('h2'));
 var About = (props) => {
     return (
         <div>
-            <h2>Welcome About page</h2>
+            <h1 className="text-center">About page</h1>
+            <p>Weather app with React</p>
+            <p>Links:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - Github react
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - Temperatures source
+                </li>
+            </ul>
         </div>
     );
 };
