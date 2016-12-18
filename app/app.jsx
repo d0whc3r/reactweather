8170fe1647f685b2d09e6f9c8ed22acb1a48d3d3
user <user@debian>
2016-12-18 11:29:23 +0100

    add sass support

--- app/app.jsx
+++ app/app.jsx
@@ -10,7 +10,7 @@ require('style!css!foundation-sites/dist/css/foundation.css');
 $(function () {
     $(document).foundation();
 });
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
